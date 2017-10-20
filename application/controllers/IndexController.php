d8a76dfd62061c11eef87f3aa6a0a4d015ad3d0b
bbrockmann1968 <bbrockmann1968@googlemail.com>
2017-10-20 11:42:37 +0200

    Änderung QM Dashboard
    Das QM Dasboard zeigt jetzt die Zertifikatsübersicht nach Zertiikat getrennt an

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -16,9 +16,7 @@ class IndexController extends Zend_Controller_Action
     {
         // action body
 		Zend_Registry::get('logger')->info('IndexController->IndexAction called');
-		$qm_dasboard['cert_exp_month'] = $this->db->query('SELECT COUNT(No) as cntcrt FROM certificate_vendor WHERE due_date between now() AND (now() + interval 1 month)')->fetchAll()[0];
-		$qm_dasboard['cert_exp'] = $this->db->query('SELECT COUNT(No) as cntcrt FROM certificate_vendor WHERE due_date < NOW()')->fetchAll()[0];
-		$qm_dasboard['certs'] = $this->db->query('SELECT COUNT(No) as cntcrt FROM certificate_vendor')->fetchAll()[0];
+		$qm_dasboard = $this->db->query('SELECT * FROM cert_status_overview')->fetchAll();
 		$ib_dashboard['inbounds_td'] = $this->db->query('SELECT COUNT(No) as cntib FROM v_inb_line WHERE inb_arrival = CURDATE()')->fetchAll()[0];
 		$whm_dashboard['last_inv_comp'] = $this->db->query('SELECT * from inventory_head WHERE state = 2 ORDER BY ended DESC')->fetchAll();
 		$whm_dashboard['act_inventory'] = $this->db->query('SELECT * FROM inventory_head where state < 2 ORDER BY started DESC')->fetchAll();
