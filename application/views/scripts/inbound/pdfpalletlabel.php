26f16ef469f44ae93379a3bd3ddda6eba7553774
bbrockmann1968 <bbrockmann1968@googlemail.com>
2017-11-10 15:35:08 +0100

    Schrift nochmal vergrößert

--- application/views/scripts/inbound/pdfpalletlabel.php
+++ application/views/scripts/inbound/pdfpalletlabel.php
@@ -20,13 +20,13 @@ try {
 		$page = new My_Pdf_Page(283, 425);
 		$page->setFont($fontBold, 45);
 		$page->drawText($this->data['vendor_name'], 283/2, 50, 'utf-8', true, My_Pdf::CENTER);
-		$page->setFont($fontBold, 40);
+		$page->setFont($fontBold, 45);
 		$page->drawText("{$this->variant['origin']} {$this->variant['product']}", round(283/2), 120, 'utf-8', true, My_Pdf::CENTER);
 		$page->drawText("{$this->data['items']}X{$this->data['weight_item']}g {$this->variant['class_short']} {$this->variant['label_short']}", round(283/2), 170, 'utf-8', true, My_Pdf::CENTER);
 		if ($this->data['lot'] == '') $this->data['lot'] = 'OLN';
 		$text = $page->drawText($this->data['lot'], 283/2, 220, 'utf-8', true, My_Pdf::CENTER);
 		$page->drawText($this->variant['brand'], round(283/2), 270, 'utf-8', true, My_Pdf::CENTER);
-		$page->setFont($fontBold, 65);
+		$page->setFont($fontBold, 70);
 		$page->drawText('P '.$this->data['position'], round(283/2), 350, 'utf-8', true, My_Pdf::CENTER);
 		$page->drawText(date('d.m.Y'), round(283/2), 415, 'utf-8', true, My_Pdf::CENTER);
 //		Zend_Barcode::factory('Ean8', 'pdf', array('text'=>$this->data['position'], 'font' => realpath(APPLICATION_PATH.'/../public/fonts/arial.ttf')), array('topOffset'=>80, 'leftOffset'=>10))->setResource($palletlabel, $p)->draw();
